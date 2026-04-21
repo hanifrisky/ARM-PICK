934aeb012cf0ecf479e9bd0849511bee064cf9fd
Hanif Samsu <hanifsamsuanggarisky@gmail.com>
2026-04-21 16:59:36 +0700

    Update CheckBox.cs

--- Assets/CheckBox.cs
+++ Assets/CheckBox.cs
@@ -5,6 +5,7 @@ public class CheckBox : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Rigidbody rb = other.GetComponent<Rigidbody>();
+        if (rb == null) return;
         rb.isKinematic = false;
     }
 }
